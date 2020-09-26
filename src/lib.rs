ad8c5b6f4922154e333a87dc0edfe169e5b4e919
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-26 14:46:25 -0400

    Add more conversions

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,14 @@
 
 #![allow(clippy::ptr_arg)]
 
-// --- Convert from &str ---
-use std::ffi::{
-    CStr, CString, FromBytesWithNulError, NulError, OsStr, OsString,
-};
+use std::borrow::Cow;
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::ffi::{FromBytesWithNulError, NulError};
 use std::path::{Path, PathBuf};
+use std::str::Utf8Error;
+use std::string::FromUtf8Error;
+
+// --- Convert from &str ---
 
 pub fn str_to_string(input: &str) -> String {
     input.to_string()
@@ -38,17 +41,16 @@ pub fn str_to_os_string(input: &str) -> OsString {
     OsStr::new(input).to_os_string()
 }
 
-// A FromBytesWithNulError will be returned if the input string is not
+// A FromBytesWithNulError will be returned if the input is not
 // nul-terminated or contains any interior nul bytes.
 //
-// If your input str is not nul-terminated then a conversion without
+// If your input is not nul-terminated then a conversion without
 // allocation is not possible, so consider using `str_to_cstring`.
 pub fn str_to_cstr(input: &str) -> Result<&CStr, FromBytesWithNulError> {
     CStr::from_bytes_with_nul(input.as_bytes())
 }
 
-// A NulError will be returned if the input string contains any nul
-// bytes.
+// A NulError will be returned if the input contains any nul bytes.
 pub fn str_to_cstring(input: &str) -> Result<CString, NulError> {
     CString::new(input)
 }
@@ -75,11 +77,84 @@ pub fn string_to_path_buf(input: String) -> PathBuf {
     PathBuf::from(input)
 }
 
-// A FromBytesWithNulError will be returned if the input string is not
+pub fn string_to_os_str(input: &String) -> &OsStr {
+    OsStr::new(input)
+}
+
+pub fn string_to_os_string(input: String) -> OsString {
+    OsString::from(input)
+}
+
+// A FromBytesWithNulError will be returned if the input is not
 // nul-terminated or contains any interior nul bytes.
 //
-// If your input String is not nul-terminated then a conversion without
+// If your input is not nul-terminated then a conversion without
 // allocation is not possible, so consider using `string_to_cstring`.
 pub fn string_to_cstr(input: &String) -> Result<&CStr, FromBytesWithNulError> {
     CStr::from_bytes_with_nul(input.as_bytes())
+}
+
+// A NulError will be returned if the input contains any nul bytes.
+pub fn string_to_cstring(input: String) -> Result<CString, NulError> {
+    CString::new(input)
+}
+
+// --- Convert from &[u8] ---
+
+// A Utf8Error will be returned if the input is not valid UTF-8.
+pub fn u8_array_to_str(input: &[u8]) -> Result<&str, Utf8Error> {
+    std::str::from_utf8(input)
+}
+
+// A FromUtf8Error will be returned if the input is not valid UTF-8.
+pub fn u8_array_to_string(input: &[u8]) -> Result<String, FromUtf8Error> {
+    String::from_utf8(input.to_vec())
+}
+
+// This never fails, but invalid UTF-8 sequences will be replaced with
+// "�". This returns a `Cow<str>`; call `to_string()` to convert it to
+// a `String`.
+pub fn u8_array_to_string_lossy(input: &[u8]) -> Cow<str> {
+    String::from_utf8_lossy(input)
+}
+
+// A direct conversion from bytes to a `Path` is only allowed on Unix.
+pub fn u8_array_to_path_unix(input: &[u8]) -> &Path {
+    use std::os::unix::ffi::OsStrExt;
+    Path::new(OsStr::from_bytes(input))
+}
+
+// A direct conversion from bytes to a `PathBuf` is only allowed on
+// Unix.
+pub fn u8_array_to_path_buf_unix(input: &[u8]) -> PathBuf {
+    use std::os::unix::ffi::OsStrExt;
+    Path::new(OsStr::from_bytes(input)).to_path_buf()
+}
+
+// A direct conversion from bytes to an `&OsStr` is only allowed on
+// Unix.
+pub fn u8_array_to_os_str_unix(input: &[u8]) -> &OsStr {
+    use std::os::unix::ffi::OsStrExt;
+    OsStr::from_bytes(input)
+}
+
+// A direct conversion from bytes to an `OsString` is only allowed on
+// Unix.
+pub fn u8_array_to_os_string_unix(input: &[u8]) -> OsString {
+    use std::os::unix::ffi::OsStringExt;
+    OsString::from_vec(input.to_vec())
+}
+
+// A FromBytesWithNulError will be returned if the input is not
+// nul-terminated or contains any interior nul bytes.
+//
+// If your input is not nul-terminated then a conversion without
+// allocation is not possible, so consider using `u8_array_to_cstring`.
+pub fn u8_array_to_cstr(input: &[u8]) -> Result<&CStr, FromBytesWithNulError> {
+    CStr::from_bytes_with_nul(input)
+}
+
+// A NulError will be returned if the input contains any nul bytes.
+pub fn u8_array_to_cstring(input: &[u8]) -> Result<CString, NulError> {
+    CString::new(input)
 }
