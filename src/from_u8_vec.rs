e90fd07a4467030a90480258896f345c765e253c
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-26 15:58:56 -0400

    Add u8_vec_to_c_str*

--- src/from_u8_vec.rs
+++ src/from_u8_vec.rs
@@ -1,6 +1,7 @@
 //! Conversions from Vec<u8>
 
-use std::ffi::{OsStr, OsString};
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::ffi::{FromBytesWithNulError, NulError};
 use std::path::{Path, PathBuf};
 use std::str::Utf8Error;
 use std::string::FromUtf8Error;
@@ -35,4 +36,20 @@ pub fn u8_vec_to_os_str_unix(input: &Vec<u8>) -> &OsStr {
 pub fn u8_vec_to_os_string_unix(input: Vec<u8>) -> OsString {
     use std::os::unix::ffi::OsStringExt;
     OsString::from_vec(input)
+}
+
+// A FromBytesWithNulError will be returned if the input is not
+// nul-terminated or contains any interior nul bytes.
+//
+// If your input is not nul-terminated then a conversion without
+// allocation is not possible, so consider using `u8_slice_to_c_string`.
+pub fn u8_vec_to_c_str(
+    input: &Vec<u8>,
+) -> Result<&CStr, FromBytesWithNulError> {
+    CStr::from_bytes_with_nul(input)
+}
+
+// A NulError will be returned if the input contains any nul bytes.
+pub fn u8_vec_to_c_string(input: Vec<u8>) -> Result<CString, NulError> {
+    CString::new(input)
 }
