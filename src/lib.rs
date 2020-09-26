e2e395bbc590bbbf4bd4efaff95b2407f3f6ff82
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-26 15:11:56 -0400

    Add Vec<u8> conversions

--- src/lib.rs
+++ src/lib.rs
@@ -128,7 +128,7 @@ pub fn u8_array_to_path_unix(input: &[u8]) -> &Path {
 // Unix.
 pub fn u8_array_to_path_buf_unix(input: &[u8]) -> PathBuf {
     use std::os::unix::ffi::OsStrExt;
-    Path::new(OsStr::from_bytes(input)).to_path_buf()
+    PathBuf::from(OsStr::from_bytes(input))
 }
 
 // A direct conversion from bytes to an `&OsStr` is only allowed on
@@ -157,4 +157,41 @@ pub fn u8_array_to_cstr(input: &[u8]) -> Result<&CStr, FromBytesWithNulError> {
 // A NulError will be returned if the input contains any nul bytes.
 pub fn u8_array_to_cstring(input: &[u8]) -> Result<CString, NulError> {
     CString::new(input)
+}
+
+// --- Convert from Vec<u8> ---
+
+pub fn u8_vec_to_str(input: &Vec<u8>) -> Result<&str, Utf8Error> {
+    std::str::from_utf8(input)
+}
+
+pub fn u8_vec_to_string(input: Vec<u8>) -> Result<String, FromUtf8Error> {
+    String::from_utf8(input)
+}
+
+// A direct conversion from bytes to a `Path` is only allowed on Unix.
+pub fn u8_vec_to_path_unix(input: &Vec<u8>) -> &Path {
+    use std::os::unix::ffi::OsStrExt;
+    Path::new(OsStr::from_bytes(input))
+}
+
+// A direct conversion from bytes to a `PathBuf` is only allowed on
+// Unix.
+pub fn u8_vec_to_path_buf_unix(input: Vec<u8>) -> PathBuf {
+    use std::os::unix::ffi::OsStringExt;
+    PathBuf::from(OsString::from_vec(input))
+}
+
+// A direct conversion from bytes to an `OsStr` is only allowed on
+// Unix.
+pub fn u8_vec_to_os_str_unix(input: &Vec<u8>) -> &OsStr {
+    use std::os::unix::ffi::OsStrExt;
+    OsStr::from_bytes(input)
+}
+
+// A direct conversion from bytes to an `OsString` is only allowed on
+// Unix.
+pub fn u8_vec_to_os_string_unix(input: Vec<u8>) -> OsString {
+    use std::os::unix::ffi::OsStringExt;
+    OsString::from_vec(input)
 }
