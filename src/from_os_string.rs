9527e9aa04ff8f27123f9761d075621fc84409f5
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-26 16:03:09 -0400

    Add os_string_to_str

--- src/from_os_string.rs
+++ src/from_os_string.rs
@@ -1,3 +1,8 @@
 //! Conversions from OsString
 
-// TODO
+use std::ffi::OsString;
+
+// None will be returned if the input is not valid UTF-8.
+pub fn os_string_to_str(input: &OsString) -> Option<&str> {
+    input.to_str()
+}
